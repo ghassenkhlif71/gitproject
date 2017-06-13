1fc35168f4956796c6c7cb829fab8e401f4e4363
GhassenKhlif <sonasony71@gmail.com>
2017-06-13 15:57:36 +0200

    added script

---
+++ Controller.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    Touch mytTouch;
+    Vector2 a;
+    // Update is called once per frame
+    
+
+    void Update () {
+
+        Debug.Log(Screen.width);
+        if (Input.touchCount > 0)
+        {
+            mytTouch = Input.GetTouch(0);
+            a = mytTouch.position;
+
+
+
+            if (a.x > Screen.width / 2)
+            {
+                transform.position +=new  Vector3(0.2f, 0,0);
+            }
+            if (a.x < Screen.width / 2)
+
+            {
+                transform.position += new Vector3(-0.2f, 0,0);
+            }
+        }
+        else
+        {
+            transform.position = new Vector2(0,0);
+        }
+    }
+}
